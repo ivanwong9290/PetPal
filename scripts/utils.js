bcc9515435100a6ad0d0d55a44599f85f674bb6f
ivanwong9290 <ivanwong9290@gmail.com>
2022-04-12 00:48:21 -0400

    4/12/22 Update
    - Included JavaScript responsive functions (to be minimize later)
    - Refactored most Bootstrap containers to CSS grid
    - Completed main page appearance
    - Added icon assets

---
+++ scripts/utils.js
@@ -0,0 +1,27 @@
+function scrollToTop() {
+    $(window).scrollTop(0);
+}
+
+function loadMorePictures() {
+    document.getElementById('hiddenGallery').style.display = "grid";
+    document.getElementById("loadButton").innerHTML = "See Less Activities";
+    document.getElementById("loadButton").onclick = function() {loadLessPictures();}
+}
+
+function loadLessPictures() {
+    document.getElementById('hiddenGallery').style.display = "none";
+    document.getElementById("loadButton").innerHTML = "See All Activities";
+    document.getElementById("loadButton").onclick = function() {loadMorePictures();}
+}
+
+function loadMorePals() {
+    document.getElementById('hiddenPals').style.display = "grid";
+    document.getElementById("loadPalsButton").innerHTML = "See Less PetPals";
+    document.getElementById("loadPalsButton").onclick = function() {loadLessPals();}
+}
+
+function loadLessPals() {
+    document.getElementById('hiddenPals').style.display = "none";
+    document.getElementById("loadPalsButton").innerHTML = "See All PetPals";
+    document.getElementById("loadPalsButton").onclick = function() {loadMorePals();}
+}
